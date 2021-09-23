3e32e4bb6fa559f963b86bc7fa1f4395c2605f20
Florieta <florieta.pencheva@gmail.com>
2021-09-23 21:45:23 +0300

    Added the git ignore file and some problem solutions about Stacks and Queues

---
+++ 01.StacksAndQueuesLab/StackSum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StackSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Stack<int> numbers = new Stack<int>(input);
+
+            string command = Console.ReadLine().ToLower();
+
+            while (command != "end")
+            {
+                string[] tokens = command.Split();
+                
+                switch (tokens[0])
+                {
+                    case "add":
+                        numbers.Push(int.Parse(tokens[1]));
+                        numbers.Push(int.Parse(tokens[2]));
+                    break;
+                    case "remove":
+                        int count = int.Parse(tokens[1]);
+                        if (numbers.Count >= count)
+                        {
+                            for (int i = 0; i < count; i++)
+                            {
+                                numbers.Pop();
+                            }
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                command = Console.ReadLine().ToLower();
+            }
+
+            Console.WriteLine($"Sum: {numbers.Sum()}");
+        }
+    }
+}
