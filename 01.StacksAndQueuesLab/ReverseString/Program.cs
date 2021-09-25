6ad6dc2b565b7a55158a1cce6d1a6aabebad1637
Florieta <florieta.pencheva@gmail.com>
2021-09-25 15:38:41 +0300

    Added some of the problem solutions about Stacks and Queues Exercise

---
+++ 01.StacksAndQueuesLab/ReverseString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StacksAndQueuesLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> charStack = new Stack<char>();
+            string input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                charStack.Push(input[i]);
+            }
+
+            while(charStack.Count > 0)
+            {
+                Console.Write(charStack.Pop());
+            }
+
+            Console.WriteLine();   
+        }
+    }
+}
