7b4638f8298f909bbdd09e14c46d6df13b5c6e1a
Florieta <florieta.pencheva@gmail.com>
2021-09-28 13:25:45 +0300

    Added some problem solutions about Multidimensional Arrays Lab

---
+++ 02.MultidimensionalArraysLab/SumMatrixColumns/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace SumMatrixColumns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = ReadArrayFromConsole();
+
+            int[,] matrix = new int[sizes[0], sizes[1]];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] arr = ReadArrayFromConsole();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = arr[col];
+                }
+            }
+
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                int sum = 0;
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    sum += matrix[row, col];
+                    
+                }
+                Console.WriteLine(sum);
+            }
+        }
+        private static int[] ReadArrayFromConsole()
+        {
+            return Console.ReadLine()
+                .Split(new string[] { ", ", " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+        }
+    }
+}
