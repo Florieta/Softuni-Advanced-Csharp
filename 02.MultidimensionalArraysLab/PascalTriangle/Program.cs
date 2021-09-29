06b9614f8496b61634d27fae5a16242ddff8bc85
Florieta <florieta.pencheva@gmail.com>
2021-09-29 16:19:21 +0300

    Added the rest of problem solutions about Multidimensional Arrays Lab

---
+++ 02.MultidimensionalArraysLab/PascalTriangle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PascalTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int height = int.Parse(Console.ReadLine());
+            long[][] triangle = new long[height][];
+
+            for (int i = 0; i < height; i++)
+            {
+                long[] row = new long[i + 1];
+                row[0] = 1;
+                row[i] = 1;
+                for (int j = 0; j < i; j++)
+                {
+                    row[j] = triangle[i - 1][j] + triangle[i - 1][j - 1];
+                }
+                triangle[i] = row;
+            }
+
+            for (int i = 0; i < height; i++)
+            {
+                Console.WriteLine(string.Join(" ", triangle[i]));
+            }
+        }
+    }
+}
---
+++ 02.MultidimensionalArraysLab/SumMatrixElements/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace MultidimensionalArraysLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = ReadArrayFromConsole();
+
+            int[,] matrix = new int[sizes[0], sizes[1]];
+            for (int row = 0; row < sizes[0]; row++)
+            {
+                int[] arr = ReadArrayFromConsole();
+                for (int col = 0; col < sizes[1]; col++)
+                {
+                    matrix[row, col] = arr[col];
+                }
+            }
+
+            Console.WriteLine(sizes[0]);
+            Console.WriteLine(sizes[1]);
+            int sum = 0;
+
+            foreach (var item in matrix)
+            {
+                sum += item;
+            }
+
+            Console.WriteLine(sum);
+        }
+
+        private static int[] ReadArrayFromConsole()
+        {
+            return Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+        }
+    }
+}
