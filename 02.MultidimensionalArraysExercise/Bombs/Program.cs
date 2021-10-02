72680c4379f9173e38f2664d6bd97ba645f257b3
Florieta <florieta.pencheva@gmail.com>
2021-10-02 13:46:29 +0300

    Added some problem solutions about Multidimensional Arrays

---
+++ 02.MultidimensionalArraysExercise/Bombs/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Linq;
+
+namespace Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = ReadMatrix(n, n);
+
+            int sum = 0;
+            int count = 0;
+
+            string[] coordinates = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            foreach (var coordinate in coordinates)
+            {
+                int[] bombIndexes = coordinate.Split(",", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                int row = bombIndexes[0];
+                int col = bombIndexes[1];
+                if (matrix[row, col] > 0)
+                {
+                    if (IsValid(row - 1, col - 1, matrix))
+                    {
+                        matrix[row - 1, col - 1] -= matrix[row, col];
+                    }
+
+                    if (IsValid(row - 1, col, matrix))
+                    {
+                        matrix[row - 1, col] -= matrix[row, col];
+                    }
+
+                    if (IsValid(row - 1, col + 1, matrix))
+                    {
+                        matrix[row - 1, col + 1] -= matrix[row, col];
+                    }
+
+                    if (IsValid(row, col - 1, matrix))
+                    {
+                        matrix[row, col - 1] -= matrix[row, col];
+                    }
+
+                    if (IsValid(row, col + 1, matrix))
+                    {
+                        matrix[row, col + 1] -= matrix[row, col];
+                    }
+
+                    if (IsValid(row + 1, col - 1, matrix))
+                    {
+                        matrix[row + 1, col - 1] -= matrix[row, col];
+                    }
+
+                    if (IsValid(row + 1, col, matrix))
+                    {
+                        matrix[row + 1, col] -= matrix[row, col];
+                    }
+
+                    if (IsValid(row + 1, col + 1, matrix))
+                    {
+                        matrix[row + 1, col + 1] -= matrix[row, col];
+                    }
+
+                    matrix[row, col] = 0;
+
+                }
+            }
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (matrix[row, col] > 0)
+                    {
+                        sum += matrix[row, col];
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Alive cells: {count}");
+            Console.WriteLine($"Sum: {sum}");
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    Console.Write($"{matrix[row, col]} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+        private static bool IsValid(int row, int col, int[,] matrix)
+        {
+            return row >= 0 && row < matrix.GetLength(0) && col >= 0 && col < matrix.GetLength(1)
+                && matrix[row, col] > 0;
+        }
+
+        static int[,] ReadMatrix(int rows, int cols)
+        {
+            int[,] matrix = new int[rows, cols];
+            for (int row = 0; row < rows; row++)
+            {
+                int[] rowValues = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = rowValues[col];
+                }
+            }
+            return matrix;
+        }
+    }
+}
---
+++ 02.MultidimensionalArraysExercise/KnightGame/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace KnightGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[][] matrix = new char[n][];
+
+            for (int row = 0; row < n; row++)
+            {
+                matrix[row] = Console.ReadLine().ToCharArray();
+
+            }
+            int removedKnights = 0;
+
+            while (true)
+            {
+                int khnightRow = -1;
+                int khnigthtCol = -1;
+                int maxAttacked = 0;
+
+                for (int row = 0; row < n; row++)
+                {
+                    for (int col = 0; col < n; col++)
+                    {
+                        if (matrix[row][col] == 'K')
+                        {
+                            int tempAttack = CountAttacks(matrix, row, col);
+                            if (tempAttack > maxAttacked)
+                            {
+                                maxAttacked = tempAttack;
+                                khnightRow = row;
+                                khnigthtCol = col;
+
+                            }
+                        }
+                }
+                }
+                if (maxAttacked > 0)
+                {
+                    matrix[khnightRow][khnigthtCol] = '0';
+                    removedKnights++;
+
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(removedKnights);
+        }
+
+        static int CountAttacks(char[][] matrix, int row, int col)
+        {
+            int attacks = 0;
+            if (IsInMatrix(row + 1, col - 2, matrix.Length) && matrix[row + 1][col - 2] == 'K')
+            {
+                attacks++;
+            }
+            if (IsInMatrix(row - 1, col - 2, matrix.Length) && matrix[row - 1][col - 2] == 'K')
+            {
+                attacks++;
+            }
+            if (IsInMatrix(row - 1, col + 2, matrix.Length) && matrix[row - 1][col + 2] == 'K')
+            {
+                attacks++;
+            }
+            if (IsInMatrix(row + 1, col + 2, matrix.Length) && matrix[row + 1][col + 2] == 'K')
+            {
+                attacks++;
+            }
+            if (IsInMatrix(row - 2, col - 1, matrix.Length) && matrix[row -2][col - 1] == 'K')
+            {
+                attacks++;
+            }
+            if (IsInMatrix(row - 2, col + 1, matrix.Length) && matrix[row - 2][col + 1] == 'K')
+            {
+                attacks++;
+            }
+            if (IsInMatrix(row + 2, col - 1, matrix.Length) && matrix[row + 2][col - 1] == 'K')
+            {
+                attacks++;
+            }
+            if (IsInMatrix(row + 2, col + 1, matrix.Length) && matrix[row + 2][col + 1] == 'K')
+            {
+                attacks++;
+            }
+            return attacks;
+        }
+
+        private static bool IsInMatrix(int row, int col, int length)
+        {
+            return row >= 0 && row < length && col >= 0 && col < length;
+        }
+    }
+}
