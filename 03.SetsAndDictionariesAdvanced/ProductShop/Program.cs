3fb670dcd3a75f9086d71a80bab6f1494e9c6e1c
Florieta <florieta.pencheva@gmail.com>
2021-10-05 13:19:23 +0300

    Added some problem solutions about Sets and Dictionaries Advance Lab

---
+++ 03.SetsAndDictionariesAdvanced/ProductShop/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProductShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            SortedDictionary<string, Dictionary<string, double>> shops = new SortedDictionary<string, Dictionary<string, double>>();
+
+            while (command != "Revision")
+            {
+                string[] data = command.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string shop = data[0];
+                string product = data[1];
+                double price = double.Parse(data[2]);
+                if (shops.ContainsKey(shop) == false)
+                {
+                    shops[shop] = new Dictionary<string, double>();
+                }
+                shops[shop].Add(product, price);
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var shop in shops)
+            {
+                Console.WriteLine($"{shop.Key}->");
+                foreach (var product in shop.Value)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+                }
+            }
+        }
+    }
+}
