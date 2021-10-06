6c005e92c80d71261e004dd3bb1e6586c75c6755
Florieta <florieta.pencheva@gmail.com>
2021-10-06 21:08:49 +0300

    Added some problem solutions about Sets and Dictionaries Advance

---
+++ 03.SetsAndDictionariesAdvanced/ParkingLot/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            HashSet<string> cars = new HashSet<string>();
+
+            while (command != "END")
+            {
+                string[] data = command.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string direction = data[0];
+                string regNumber = data[1];
+                if (direction == "IN")
+                {
+                    cars.Add(regNumber);
+                }
+                else
+                {
+                    cars.Remove(regNumber);
+                }
+                command = Console.ReadLine();
+
+            }
+
+            if (cars.Count > 0)
+            {
+                foreach (var car in cars)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Parking Lot is Empty");
+            }
+            
+
+            
+        }
+    }
+}
---
+++ 03.SetsAndDictionariesAdvanced/SoftUniParty/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool partyStarted = false;
+            HashSet<string> VIPguests = new HashSet<string>();
+            HashSet<string> regularGuests = new HashSet<string>();
+            string command;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                if (command == "PARTY")
+                {
+                    partyStarted = true;
+                    
+                    continue;
+                }
+
+                if(partyStarted)
+                {
+                    if (IsVip(command))
+                    {
+                        VIPguests.Remove(command);
+                    }
+                    else
+                    {
+                        regularGuests.Remove(command);
+                    }
+                }
+                else
+                {
+                    if (IsVip(command))
+                    {
+                        VIPguests.Add(command);
+                    }
+                    else
+                    {
+                        regularGuests.Add(command);
+                    }
+                }
+                
+            }
+            Console.WriteLine(VIPguests.Count + regularGuests.Count);
+
+            foreach (var person in VIPguests)
+            {
+                Console.WriteLine(person);
+            }
+
+            foreach (var person in regularGuests)
+            {
+                Console.WriteLine(person);
+            }
+        }
+
+
+
+        private static bool IsVip (string number)
+        {
+            int num = 0;
+
+            return int.TryParse(number.Substring(0, 1), out num);
+        }
+    }
+}
