8c2dc8031c161939c005a4bc62e1e5e38143b302
Florieta <florieta.pencheva@gmail.com>
2021-10-08 14:17:19 +0300

    Added some problem solutions about Sets and Dictionaries Exercise

---
+++ 03.SetsAndDictionariesAdvancedExercise/SetsOfElements/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SetsOfElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lenghts = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            HashSet<int> first = new HashSet<int>();
+            HashSet<int> second = new HashSet<int>();
+            FillSets(first, lenghts[0]);
+            FillSets(second, lenghts[1]);
+            CheckSets(first, second);
+
+        }
+
+        static void CheckSets(HashSet<int> first, HashSet<int> second)
+        {
+            List<int> nums = new List<int>();
+            foreach (var number in first)
+            {
+                if(second.Contains(number))
+                {
+                    nums.Add(number);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", nums));
+        }
+
+        static HashSet<int> FillSets(HashSet<int> set, int lenght)
+        {
+            for (int i = 0; i < lenght; i++)
+            {
+                set.Add(int.Parse(Console.ReadLine()));
+            }
+
+            return set;
+        }
+    }
+}
---
+++ 03.SetsAndDictionariesAdvancedExercise/UniqueUsernames/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SetsAndDictionariesAdvancedExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> usernames = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string user = Console.ReadLine();
+                usernames.Add(user);
+            }
+
+            foreach (var user in usernames)
+            {
+                Console.WriteLine(user);
+            }
+        }
+    }
+}
