94080904c9a7557abf4d1903b6feaf5b2dc96d24
Florieta <florieta.pencheva@gmail.com>
2021-10-09 14:12:43 +0300

    Added some problem solutions about Sets and Dictionaries Advance

---
+++ 03.SetsAndDictionariesAdvancedExercise/EvenTimes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<int, int> counter = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (!counter.ContainsKey(number))
+                {
+                    counter.Add(number, 0);
+                }
+
+                counter[number]++;
+
+            }
+
+            foreach (var number in counter)
+            {
+                if (number.Value % 2 == 0)
+                {
+                    Console.WriteLine(number.Key);
+                }
+            }
+        }
+    }
+}
---
+++ 03.SetsAndDictionariesAdvancedExercise/PeriodicTable/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            SortedSet<string> elements = new SortedSet<string>();
+            FillSet(elements, n);
+            Console.WriteLine(string.Join(" ", elements));
+        }
+
+        private static SortedSet<string> FillSet(SortedSet<string> elements, int number)
+        {
+            
+            for (int i = 0; i < number; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                for (int k = 0; k < input.Length; k++)
+                {
+                    elements.Add(input[k]);
+                }
+            }
+            
+            return elements;
+        }
+    }
+}
