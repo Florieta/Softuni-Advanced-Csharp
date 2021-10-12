f0cc113ed7f357a6f5f80eea669cdc9311a9a2e1
Florieta <florieta.pencheva@gmail.com>
2021-10-12 13:07:23 +0300

    Added the resources and some problem solutions about Streams, Files and Directories Lab

---
+++ 04.StreamsFilesAndDirectoriesLab/LineNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace LineNumbers
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (StreamReader srt = new StreamReader("Input.txt"))
+            {
+                string line = await srt.ReadLineAsync();
+                using(StreamWriter wrt = new StreamWriter("Output.txt"))
+                {
+                    int count = 1;
+                    while (line != null)
+                    {
+                        await wrt.WriteLineAsync($"{count}. {line}");
+                        count++;
+                        line = await srt.ReadLineAsync();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 04.StreamsFilesAndDirectoriesLab/OddLines/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace StreamsFilesAndDirectoriesLab
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (StreamReader str = new StreamReader("Input.txt"))
+            {
+                int currLine = 0;
+
+                string line = await str.ReadLineAsync();
+
+                while (line != null)
+                {
+                    if (currLine % 2 != 0)
+                    {
+                        Console.WriteLine(line);
+                    }
+                    currLine++;
+                    line = await str.ReadLineAsync();
+                }
+            }
+        }
+    }
+}
