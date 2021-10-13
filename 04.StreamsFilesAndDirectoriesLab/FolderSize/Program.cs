13adbcf7396af7e2268fbb4ccd80d18e7717723a
Florieta <florieta.pencheva@gmail.com>
2021-10-13 11:59:55 +0300

    Added some problem solutions about Streams, Files and Directories

---
+++ 04.StreamsFilesAndDirectoriesLab/FolderSize/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace FolderSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fileNames = Directory.GetFiles("c:/TestFolder");
+            double totalSize = 0;
+
+            foreach (var filename in fileNames)
+            {
+                FileInfo info = new FileInfo(filename);
+                totalSize += info.Length;
+            }
+
+            totalSize = totalSize / 1024 / 1024;
+
+            File.WriteAllText("оutput.txt", totalSize.ToString());
+        }
+    }
+}
