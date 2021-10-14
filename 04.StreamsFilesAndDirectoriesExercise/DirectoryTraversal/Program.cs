a08f2c1990a9aba07e75651ccbe571b93e23817f
Florieta <florieta.pencheva@gmail.com>
2021-10-14 21:28:20 +0300

    Added some problem solutions about Streams, Files and Directories

---
+++ 04.StreamsFilesAndDirectoriesExercise/DirectoryTraversal/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DirectoryTraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<FileInfo>> filesByExtension = new Dictionary<string, List<FileInfo>>();
+            string path = Console.ReadLine();
+            string[] files = Directory.GetFiles(path);
+
+            foreach (string file  in files)
+            {
+                FileInfo info = new FileInfo(file);
+                string extension = info.Extension;
+                if(!filesByExtension.ContainsKey(extension))
+                {
+                    filesByExtension[extension] = new List<FileInfo>();
+                }
+                filesByExtension[extension].Add(info);
+            }
+            using (StreamWriter writer = new StreamWriter(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/report.txt"))
+            {
+                foreach (var kvp in filesByExtension.OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key))
+                {
+                    writer.WriteLine(kvp.Key);
+                    foreach (var fileInfo in kvp.Value.OrderByDescending(x => Math.Ceiling((double)x.Length/1024)))
+                    {
+                        writer.WriteLine($"--{fileInfo.Name} - {Math.Ceiling((double)fileInfo.Length / 1024)}");
+                    }
+                }
+            }
+        }
+    }
+}
