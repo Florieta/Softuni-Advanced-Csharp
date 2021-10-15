3920bffc135bd9ec0809fa247fea12247a9769ca
Florieta <florieta.pencheva@gmail.com>
2021-10-15 12:25:35 +0300

    Added some problem solutions about Files, Streams and Directories

---
+++ 04.StreamsFilesAndDirectoriesExercise/EvenLines/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace EvenLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] charToReplace = new char[] { '-', ',', '.', '!', '?' };
+
+            using StreamReader reader = new StreamReader("text.txt");
+            int count = 0;
+
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (line == null)
+                {
+                    break;
+                }
+
+                if (count % 2 == 0)
+                {
+                   line = ReplaceAll(charToReplace, '@', line);
+                    line = Reverse(line);
+                    Console.WriteLine(line);
+                }
+                count++;
+            }
+
+        }
+
+        static string Reverse(string line)
+        {
+            StringBuilder sb = new StringBuilder();
+            string[] word = line.Split().ToArray();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                sb.Append(word[word.Length - i - 1]);
+                sb.Append(' ');
+            }
+            return sb.ToString().ToString().TrimEnd();
+        }
+
+        static string ReplaceAll(char[] charToReplace, char v, string line)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < line.Length; i++)
+            {
+                char currentSymbol = line[i];
+                if (charToReplace.Contains(currentSymbol))
+                {
+                    sb.Append('@');
+                }
+                else
+                {
+                    sb.Append(currentSymbol);
+                }
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 04.StreamsFilesAndDirectoriesExercise/LineNumbers/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("text.txt");
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string currentLine = lines[i];
+                int lettersCount = CountOfLetters(currentLine);
+                int punctionalCount = CountOfPunctional(currentLine);
+                lines[i] = $"Line {i + 1}: {currentLine}. ({lettersCount})({punctionalCount})";
+            }
+            File.WriteAllLines("../../../Output.txt", lines);
+        }
+
+        static int CountOfPunctional(string currentLine)
+        {
+            int count = 0;
+            char[] punctionlArr = new char[] { '-', ',', '.', '!', '?' };
+            for (int i = 0; i < currentLine.Length; i++)
+            {
+                char currChar = currentLine[i];
+                if (char.IsPunctuation(currChar))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        static int CountOfLetters(string currentLine)
+        {
+            int count = 0;
+
+            for (int i = 0; i < currentLine.Length; i++)
+            {
+                char currChar = currentLine[i];
+                if (char.IsLetter(currChar))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
