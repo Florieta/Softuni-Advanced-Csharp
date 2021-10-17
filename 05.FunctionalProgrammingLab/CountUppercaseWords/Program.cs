1cbaf22157c8ee02c43dfcb761a86a32d3e9568a
Florieta <florieta.pencheva@gmail.com>
2021-10-17 13:02:46 +0300

    Added some problem solutions about Functional Programming Lab

---
+++ 05.FunctionalProgrammingLab/CountUppercaseWords/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<string> predicate = str => str[0] == str.ToUpper()[0];
+            //Func<string, bool> predicate = str => str[0] == str.ToUpper()[0];
+
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(w => predicate(w))
+                .ToList()
+                .ForEach(w => Console.WriteLine(w));
+        }
+    }
+}
---
+++ 05.FunctionalProgrammingLab/SortEvenNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace FunctionalProgramingLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Where(n => n % 2 == 0)
+                .OrderBy(n => n).ToArray();
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
