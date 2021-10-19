0ae5a1b95cd0b2007720e3ba63b4df35d558e66a
Florieta <florieta.pencheva@gmail.com>
2021-10-19 15:35:32 +0300

    Added some problem solutions about Functional Programming

---
+++ 05.FunctionalProgrammingExercise/Action Print/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace FunctionalProgrammingExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> names = name => Console.WriteLine(name);
+
+            Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList().ForEach(names);
+        }
+    }
+}
---
+++ 05.FunctionalProgrammingExercise/KnightsOfHonor/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList().ForEach(name => Console.WriteLine("Sir " + name));
+        }
+    }
+}
