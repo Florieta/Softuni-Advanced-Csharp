55107188888e7972b68ddf334880104d8affd901
Florieta <florieta.pencheva@gmail.com>
2021-10-20 10:39:15 +0300

    Added some problem solutions about Functional Programming

---
+++ 05.FunctionalProgrammingExercise/CustomMinFunction/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var input = Console.ReadLine().Split().Select(int.Parse).ToList();
+            //Console.WriteLine(input.Min());
+
+            Func<int[], int> minNumber = n => n.Min();
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(minNumber(numbers));
+        }
+    }
+}
---
+++ 05.FunctionalProgrammingExercise/FindEvensOrOdds/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FindEvensOrOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] bounds = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            string querry = Console.ReadLine();
+
+            Predicate<int> predicate = querry == "odd"
+                ? new Predicate<int>((number) => number % 2 != 0)
+                : new Predicate<int>((number) => number % 2 == 0);
+
+            var result = new List<int>();
+
+            for (int i = bounds[0]; i <= bounds[1]; i++)
+            {
+                if (predicate(i))
+                {
+                    result.Add(i);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
