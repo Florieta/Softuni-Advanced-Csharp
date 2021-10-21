ae79315c84c18a3ba58976d19688049e26e3e641
Florieta <florieta.pencheva@gmail.com>
2021-10-21 08:19:12 +0300

    Added some problem solutions about Functional Programming Exercise

---
+++ 05.FunctionalProgrammingExercise/AppliedArithmetics/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace AppliedArithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            Action<int[]> printer = number => Console.WriteLine(string.Join(" ", number));
+            string commands = Console.ReadLine();
+
+            while (commands != "end")
+            {
+                if (commands == "add")
+                {
+                    numbers = ForEach(numbers, number => ++number);
+                }
+                else if (commands == "multiply")
+                {
+                    numbers = ForEach(numbers, number => number * 2);
+                }
+                else if (commands == "subtract")
+                {
+                    numbers = ForEach(numbers, number => --number);
+                }
+                else if (commands == "print")
+                {
+                    printer(numbers);
+                }
+                commands = Console.ReadLine();
+
+            }
+        }
+
+        public static int[] ForEach(int[] numbers, Func<int, int> func)
+        => numbers.Select(number => func(number)).ToArray();
+    }
+}
---
+++ 05.FunctionalProgrammingExercise/ReverseAndExclude/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int divisibleNumber = int.Parse(Console.ReadLine());
+            Predicate<int> filter = numbers => numbers % divisibleNumber != 0;
+            Action<int[]> printer = number => Console.WriteLine(string.Join(" ", number.Reverse().Where(x => filter(x))));
+            printer(numbers);
+        }
+    }
+}
