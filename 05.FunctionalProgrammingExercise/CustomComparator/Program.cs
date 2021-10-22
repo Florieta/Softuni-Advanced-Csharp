9d94ec748d665517af7a888c466d4bd32299310b
Florieta <florieta.pencheva@gmail.com>
2021-10-22 10:28:43 +0300

    Added some problem solutions about Functional Programming Exercise

---
+++ 05.FunctionalProgrammingExercise/CustomComparator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomComparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int, int, int> comparator = (number1, number2) => (number1 % 2 == 0 && number2 % 2 != 0) ? -1 : 
+            (number1 % 2 != 0 && number2 % 2 == 0) ? 1 : number1.CompareTo(number2);
+
+            Array.Sort<int>(numbers, new Comparison<int>(comparator));
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
---
+++ 05.FunctionalProgrammingExercise/PredicateForNames/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split();
+            Action<string[]> printNames = name =>
+            {
+                Predicate<string> filter = name => name.Length <= length;
+                foreach (string currName in names.Where(name => filter(name)))
+                {
+                    Console.WriteLine(currName);
+                }
+            };
+
+            printNames(names);
+        }
+    }
+}
