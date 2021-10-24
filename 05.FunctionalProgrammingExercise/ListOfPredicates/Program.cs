b089d565b2dcae85686725f142723713678dc892
Florieta <florieta.pencheva@gmail.com>
2021-10-24 12:22:35 +0300

    Added some problem solutions about Functional Programming

---
+++ 05.FunctionalProgrammingExercise/ListOfPredicates/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int range = int.Parse(Console.ReadLine());
+            int[] dividers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            if (range <= 0)
+            {
+                return;
+            }
+
+            Func<int[], int, bool> filter = (alldividers, number) =>
+            {
+                bool divisible = true;
+
+                for (int i = 0; i < alldividers.Length; i++)
+                {
+                    if (number % alldividers[i] != 0)
+                    {
+                        divisible = false;
+                        break;
+                    }
+                }
+
+                return divisible;
+            };
+
+            int[] divisibleNumbers = Enumerable.Range(1, range).Where(number => filter(dividers, number)).ToArray();
+
+            Console.WriteLine(string.Join(" ", divisibleNumbers));
+        }
+    }
+}
---
+++ 05.FunctionalProgrammingExercise/TriFunction/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split();
+
+            Func<string, int, bool> validator = (name, value) => name.ToCharArray().Select(currChar => (int)currChar).Sum() >= n;
+            Func<string[], int, Func<string, int, bool>, string> foundName = (collection, value, func) => collection.FirstOrDefault(name => func(name, value));
+
+            Console.WriteLine(foundName(names, n, validator));
+        }
+    }
+}
