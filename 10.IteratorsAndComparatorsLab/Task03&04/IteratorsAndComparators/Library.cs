d889286c0626cbcdc277ae5267fefe4a98bbf2ae
Florieta <florieta.pencheva@gmail.com>
2021-11-07 21:17:22 +0200

    Added some problem solutions about Iterators and Comparators

---
+++ 10.IteratorsAndComparatorsLab/Task03&04/IteratorsAndComparators/Library.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private readonly List<Book> books;
+
+        public Library(params Book[] _books)
+        {
+            books = new List<Book>(_books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return books.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ 10.IteratorsAndComparatorsLab/Task03&04/LibraryIterartor/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = new List<string>(authors);
+        }
+
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public IReadOnlyList<string> Authors { get; set; }
+
+        public int CompareTo(Book other)
+        {
+            if (this.Year != other.Year)
+            {
+                return this.Year.CompareTo(other.Year);
+            }
+            return this.Title.CompareTo(other.Title);
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Year}";
+        }
+    }
+}
