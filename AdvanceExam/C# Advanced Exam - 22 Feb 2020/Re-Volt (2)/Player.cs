caf2481a61bf3640661ca4b9d2794e91211d9abb
Florieta <florieta.pencheva@gmail.com>
2021-11-12 21:54:45 +0200

    Added some examples of problem solutions about the final exam

---
+++ AdvanceExam/C# Advanced Exam - 22 Feb 2020/Re-Volt (2)/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guild
+{
+    public class Player
+    {
+        public Player(string name, string @class)
+        {
+            Name = name;
+            Class = @class;
+            Rank = "Trial";
+            Description = "n/a";
+        }
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Rank { get; set; }
+        public string Description { get; set; }
+        public override string ToString()
+        {
+            return $"Player {Name}: {Class}\r\n" +
+                $"Rank: {Rank}\r\n" +
+                $"Description: {Description}";
+        }
+    }
+}
+
+
+
+
+
---
+++ AdvanceExam/C# Advanced Exam - 22 Feb 2020/Re-Volt (3)/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LootBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> firstBox = new Queue<int>(Console.ReadLine().Split(" ").Select(int.Parse).ToArray());
+            Stack<int> secondBox = new Stack<int>(Console.ReadLine().Split(" ").Select(int.Parse).ToArray());
+           
+            int sum = 0;
+            
+            while (firstBox.Count > 0 && secondBox.Count > 0)
+            {
+                int result = firstBox.Peek() + secondBox.Peek();
+                if (result % 2 == 0)
+                {
+                    sum += result;
+                    firstBox.Dequeue();
+                    secondBox.Pop();
+                }
+                else
+                {
+                    firstBox.Enqueue(secondBox.Pop());
+                }
+            }
+
+            if (firstBox.Count == 0)
+            {
+                Console.WriteLine("First lootbox is empty");
+            }
+            else if (secondBox.Count == 0)
+            {
+                Console.WriteLine("Second lootbox is empty");
+            }
+
+            if (sum >= 100)
+            {
+                Console.WriteLine($"Your loot was epic! Value: {sum}");
+            }
+            else
+            {
+                Console.WriteLine($"Your loot was poor... Value: {sum}");
+            }
+        }
+    }
+}
