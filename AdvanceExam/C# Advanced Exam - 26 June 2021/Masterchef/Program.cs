40bc6962614bae3e30f4bd169254ebda0a2adf67
Florieta <florieta.pencheva@gmail.com>
2021-11-15 21:36:35 +0200

    Added some problem solutions about Final Exam

---
+++ AdvanceExam/C# Advanced Exam - 26 June 2021/Masterchef/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Masterchef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> ingredients = new Queue<int>(Console.ReadLine().Split(" ").Select(int.Parse).ToArray());
+            Stack<int> freshness = new Stack<int>(Console.ReadLine().Split(" ").Select(int.Parse).ToArray());
+            SortedDictionary<string, int> data = new SortedDictionary<string, int>(); Dictionary<string, int> menu = new Dictionary<string, int>
+            {
+                {"Chocolate cake", 0},
+                {"Dipping sauce", 0},
+                {"Green salad", 0},
+                {"Lobster", 0}
+            };
+           
+            string cake = "Chocolate cake";
+            string sauce = "Dipping sauce";
+            string salad = "Green salad";
+            string lobster = "Lobster";
+
+
+
+            while (ingredients.Count > 0 && freshness.Count > 0)
+            {
+                int currentIngredient = ingredients.Peek();
+                int currentFreshness = freshness.Peek();
+                bool success = false;
+
+                if (currentIngredient <= 0)
+                {
+                    ingredients.Dequeue();
+                    continue;
+                }
+
+                int cooking = currentIngredient * currentFreshness;
+
+                if (cooking.Equals(150))
+                {
+                    success = true;
+                    menu[sauce]++;
+                }
+                else if (cooking.Equals(250))
+                {
+                    success = true;
+                    menu[salad]++;
+                }
+                else if (cooking.Equals(300))
+                {
+                    success = true;
+                    menu[cake]++;
+                }
+                else if (cooking.Equals(400))
+                {
+                    success = true;
+                    menu[lobster]++;
+                }
+                else
+                {
+                    freshness.Pop();
+                    ingredients.Enqueue(ingredients.Dequeue() + 5);
+                }
+
+                if (success)
+                {
+                    ingredients.Dequeue();
+                    freshness.Pop();
+                }
+
+            }
+            menu = menu
+                .Where(x => x.Value != 0)
+                .ToDictionary(x => x.Key, y => y.Value);
+
+            Console.WriteLine(menu.Keys.Count == 4
+                ? "Applause! The judges are fascinated by your dishes!"
+                : "You were voted off. Better luck next year.");
+
+            if (ingredients.Any())
+            {
+                Console.WriteLine($"Ingredients left: {ingredients.Sum()}");
+            }
+            foreach (var meal in menu.Where(x => x.Value > 0))
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, $" # {meal.Key} --> {meal.Value}"));
+            }
+            
+        }
+    }
+}
---
+++ AdvanceExam/C# Advanced Exam - 26 June 2021/SkiRental/SkiRental.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkiRental
+{
+    class SkiRental
+    {
+        private readonly List<Ski> data;
+
+        public SkiRental(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            data = new List<Ski>();
+        }
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Count => data.Count;
+
+        public void Add(Ski ski)
+        {
+            if (data.Count < Capacity)
+            {
+                data.Add(ski);
+            }
+        }
+
+        public bool Remove(string manufacturer, string model)
+        {
+            return data.Remove(data.FirstOrDefault(x => x.Manufacturer == manufacturer && x.Model == model));
+        }
+
+        public Ski GetNewestSki()
+        {
+            return data.OrderByDescending(x => x.Year).FirstOrDefault();
+        }
+        public Ski GetSki(string manufacturer, string model)
+        {
+            return data.FirstOrDefault(x => x.Manufacturer == manufacturer && x.Model == model);
+        }
+        public string GetStatistics()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"The skis stored in {Name}:");
+            for (int i = 0; i < data.Count; i++)
+            {
+                sb.AppendLine($"{data[i]}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
