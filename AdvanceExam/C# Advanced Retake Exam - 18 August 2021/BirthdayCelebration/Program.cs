905e167012af08b074ba7f1602ef9e4f568f09b8
Florieta <florieta.pencheva@gmail.com>
2021-11-19 21:26:09 +0200

    Added some problem solutions about Final Exam

---
+++ AdvanceExam/C# Advanced Retake Exam - 18 August 2021/BirthdayCelebration/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BirthdayCelebration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> guestsCapacity = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).Reverse());
+            Stack<int> plates = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            int wastedFood = 0;
+
+            while (guestsCapacity.Count > 0 && plates.Count > 0)
+            {
+                if (guestsCapacity.Peek() - plates.Peek() <= 0)
+                {
+                    wastedFood += Math.Abs(guestsCapacity.Peek() - plates.Peek());
+                    guestsCapacity.Pop();
+                }
+                else
+                {
+                    guestsCapacity.Push(guestsCapacity.Pop() - plates.Peek());
+                }
+                plates.Pop();
+            }
+
+            if (guestsCapacity.Count > 0)
+            {
+                Console.WriteLine($"Guests: {string.Join(" ", guestsCapacity)}");
+            }
+            else if (plates.Count > 0)
+            {
+                Console.WriteLine($"Plates: {string.Join(" ", plates)}");
+            }
+
+            Console.WriteLine($"Wasted grams of food: {wastedFood}");
+        }
+    }
+}
---
+++ AdvanceExam/C# Advanced Retake Exam - 18 August 2021/StreetRacing/Race.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StreetRacing
+{
+    public class Race
+    {
+        private readonly List<Car> Participants;
+
+        public Race(string name, string type, int laps, int capacity, int maxHorsePower)
+        {
+            Name = name;
+            Type = type;
+            Laps = laps;
+            Capacity = capacity;
+            MaxHorsePower = maxHorsePower;
+            Participants = new List<Car>();
+        }
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public int Laps { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int MaxHorsePower { get; set; }
+
+        public int Count => Participants.Count;
+
+        public void Add(Car car)
+        {
+            bool isExisted = false;
+            foreach (Car participant in Participants)
+            {
+                if (participant.LicensePlate == car.LicensePlate)
+                {
+                    isExisted = true;
+                }
+            }
+
+            if (!isExisted)
+            {
+                if (Participants.Count < Capacity && car.HorsePower <= MaxHorsePower)
+                {
+                    Participants.Add(car);
+                }
+            }
+        }
+        public bool Remove(string licensePlate)
+        {
+            return Participants.Remove(Participants.FirstOrDefault(x => x.LicensePlate == licensePlate));
+        }
+        public Car FindParticipant(string licensePlate)
+        {
+            return Participants.FirstOrDefault(x => x.LicensePlate == licensePlate);
+        }
+
+        public Car GetMostPowerfulCar()
+        {
+            return Participants.OrderByDescending(x => x.HorsePower).FirstOrDefault();
+        }
+        public string Report()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Race: {Name} - Type: {Type} (Laps: {Laps})");
+            for (int i = 0; i < Participants.Count; i++)
+            {
+                sb.AppendLine($"{Participants[i]}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
